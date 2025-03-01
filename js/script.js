6c983d887cdbc9c5235012214caa5ac5270df90c
zingrin <jerinloncheu@gmail.com>
2025-03-02 04:54:02 +0600

    complete all

--- js/script.js
+++ js/script.js
@@ -1,22 +1,48 @@
+document.getElementById("themchange").addEventListener("click", function(event){
+    document.body.style.backgroundColor = getRandomHexColor();
+})
+
 // go to discover
-document.getElementById("btn-discover").addEventListener("click", function(event){
+document.getElementById("btn-discover").addEventListener("click", function (event) {
     event.preventDefault();
 
-window.location.href = "./blog.html"
+    window.location.href = "./blog.html"
 })
 getDate()
 
 // initial count
+let massage = 0;
 let assignCount = 6;
+let navCount = 23;
+document.getElementById("btn-count").innerText = navCount;
 document.getElementById("taskAssign").innerText = assignCount;
 
 
 const allButtons = document.querySelectorAll(".completed");
 
-for(let btn of allButtons){
-    btn.addEventListener("click", function(event){
+for (let btn of allButtons) {
+    btn.addEventListener("click", function (event) {
+        alert('Board Updated successfully')
 
-console.log(event.target)
+
+        assignCount--;
+        navCount++;
+        document.getElementById("btn-count").innerText = navCount;
+
+        document.getElementById("taskAssign").innerText = assignCount;
+        const time = updateTime();
+        let title = btn.parentElement.parentElement.querySelector(".card-title").innerText;
+        const historyContainer = document.getElementById("history")
+        const div = document.createElement("div");
+        div.innerHTML = `
+<h1> You have completed the task ${title} ${time} </h1>
+`
+        historyContainer.appendChild(div)
+
+        btn.setAttribute("disabled", "true")
+        if (massage === assignCount) {
+            alert('Congrats!!! You have completed all the current tasks.')
+        }
     })
 }
 
@@ -24,4 +50,8 @@ console.log(event.target)
 
 
 
+document.getElementById("clearHistory").addEventListener("click", function (event) {
+    clearHistory();
+    event.preventDefault
+})
 
--- utilities/utilities.js
+++ utilities/utilities.js
@@ -9,6 +9,12 @@ window.location.href = "./index.html"
 
 }
 backDesk()
+function getRandomHexColor() {
+    let hex = "#" + Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0");
+    return hex;
+}
+
+
 function getDate(){
 const date = new Date();
 
@@ -23,3 +29,29 @@ document.getElementById("fullDate").innerText =`
 `
 }
 
+function updateTime() {
+    const now = new Date();
+
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    let ampm = hours >= 12 ? "PM" : "AM";
+
+    hours = hours % 12 || 12;
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    let timeString = `${hours}:${minutes}:${seconds} ${ampm}`;
+return timeString;
+}
+
+updateTime();
+
+
+
+
+function clearHistory(){
+    const historyContainer = document.getElementById("history")
+historyContainer.innerHTML = ""
+}
